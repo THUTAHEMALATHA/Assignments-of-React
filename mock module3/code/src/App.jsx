81160d9cf79e6702c289376e47993960e152d2a5
THUTAHEMALATHA <thutahemalatha@gmail.com>
2025-12-21 18:55:11 +0530

    coding

--- mock module3/code/src/App.jsx
+++ mock module3/code/src/App.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 
 const Header=()=>{
-  const{}
+  const{ theme, toggleTheme} = useTheme();
   return(
+    <header className="header">
+      <h1>Posts App</h1>
+      <button onClick={toggleTheme}>Switch Theme({theme === "light" ? "Light -> Dark" : "dark -> Light"}) </button>
 
+    </header>
   );
-}
+};
+
+const AppInner = () =>{
+     <PostsProvider>
+      <div className="app">
+        <Header />
+      </div>
+     </PostsProvider>
+};
+
+const App = () =>{
+  <ThemeProvider>
+    <AppInner/>
+  </ThemeProvider>
+};
+export default App;
