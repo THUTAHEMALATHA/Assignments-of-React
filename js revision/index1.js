f698476e44c36c33d315c4b5da1cdcec1a08913c
hemalatha <thutahemalatha@gmail.com>
2026-03-07 22:03:16 +0530

    sum of natural numbers

---
+++ js revision/index1.js
@@ -0,0 +1,17 @@
+var n=Number(prompt("enter your Number"))
+
+if(isNaN(n)){
+    console.log("Invalid input");
+    
+}
+if(n>0){
+    var sum = 0;
+    for(var i=1; i<=n; i++){
+        sum = sum +i;
+        console.log(sum);
+        
+    }
+}else {
+    console.log("negative number or morethan 0 is not valid here");
+    
+};
