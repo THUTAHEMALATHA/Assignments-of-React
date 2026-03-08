3e3a4c09b2b015063eed60292f506b3f839a8b59
hemalatha <thutahemalatha@gmail.com>
2026-03-08 15:09:40 +0530

    refresh

---
+++ revision/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const supabase = require('../supabaseClient')
+
+const app = express();
+app.use(express.json());
+
+// app.get('/courses', async(req, res)
+// =>{
+//     const {data,error} =await supabase
+//     .from('courses')
+//     .select('*')
+//     res.json(data)
+// })
+
+app.listen(5000, () => {
+    console.log("server running on port 5000")
+})
